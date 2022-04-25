34cf10a385fa3222572986fbceee6ab4fdfeba4d
bang9 <gusrn1423@naver.com>
2022-04-25 10:42:59 +0900

    skip: migrate sample

---
+++ sample/ios/SendbirdCallsRNSample-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SendbirdCallsRNSample-Bridging-Header.h
+//  SendbirdCallsRNSample
+//
+//  Created by Airen Kang on 2022/04/21.
+//
+
+#ifndef SendbirdCallsRNSample_Bridging_Header_h
+#define SendbirdCallsRNSample_Bridging_Header_h
+
+
+#endif /* SendbirdCallsRNSample_Bridging_Header_h */
